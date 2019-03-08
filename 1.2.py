a03ae08797322c069ae87fc441f5e48a35d1b176
ghazalb76 <ghazal.b93@gmail.com>
2019-03-08 20:23:38 +0330

    Implement second part

---
+++ 1.2.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+j = np.complex(0, 1)
+pi = np.pi
+
+#continuous
+x = np.arange(-0, 2.01, 0.1)
+y1 = np.cos(2*pi*x)
+y2 = np.sin(pi*x)
+y = y1+y2
+plt.plot(x,y)
+
+#discrete
+x = np.arange(-0, 2.01, 1)
+y1 = np.cos(2*pi*x)
+y2 = np.sin(pi*x)
+y = y1+y2
+plt.stem(x,y)
+
+#show
+plt.show()
+
+# T = 2 but i have set 2.01 for range to see the exact 2 too
