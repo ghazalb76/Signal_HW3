115c693eaacf849f78897aac58b24b1e57a44775
ghazalb76 <ghazal.b93@gmail.com>
2019-03-08 23:06:18 +0330

    Complete some parts of question6

--- 6.py
+++ 6.py
@@ -4,7 +4,7 @@ from scipy import signal
 
 def r(n):
     a = []
-    for i in range(-30, 30):
+    for i in range(-10, 10):
         if i<n:
             a.append(0)
             # print(i, ":", 0)
@@ -15,7 +15,7 @@ def r(n):
 
 def u(n):
     a = []
-    for i in range(-30, 30):
+    for i in range(-10, 10):
         if i<n:
             a.append(0)
             # print(i, ":", 0)
@@ -24,13 +24,24 @@ def u(n):
             # print(i, ":", 1)
     return a
 
+
+# First signal
+chart1 = plt.subplot(311)
+x1 = np.arange(-10, 10, 1)
 x = [x11 - x22 for (x11, x22) in zip(u(0), u(12))]
+plt.plot(x1, x)
+
+# Second signal
+chart2 = plt.subplot(312)
+x2 = np.arange(-10, 10, 1)
 y = [y11 - y22 - y33 - y44 for (y11, y22, y33, y44) in zip(r(0), r(4), r(12), r(16))]
+plt.stem(x2, y)
 
-recorded = signal.convolve(x, y)
-x = np.arange(-30, 30, 0.1)
-plt.plot(x,recorded)
-
+# Deconvolution
+chart3 = plt.subplot(313)
+x = np.arange(-19, 20, 1)
+recovered, remainder = signal.deconvolve(x, y)
+plt.stem(x,recovered)
 
 # Show
 plt.show()
