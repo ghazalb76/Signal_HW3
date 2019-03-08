222b223da7df3022ce6755fb991ce16eb8e90703
ghazalb76 <ghazal.b93@gmail.com>
2019-03-08 13:29:29 +0330

    Seperate charts

--- 4.py
+++ 4.py
@@ -7,6 +7,37 @@ print(m)
 pi = np.pi
 
 # Continuous
+chart1 = plt.subplot(311)
+x = np.arange(-30, 30, 0.01)
+y1 = np.sin(2*pi*0.043*x)
+y2 = np.sin(2*pi*0.031*x)
+y = y1+y2
+plt.plot(x,y)
+
+# Discrete
+x = np.arange(-30, 30, 1)
+y1 = np.sin(2*pi*0.043*x)
+y2 = np.sin(2*pi*0.031*x)
+y = y1+y2
+plt.stem(x,y)
+
+# Continuous with down sampling
+chart2 = plt.subplot(312)
+x = np.arange(-30, 30, 0.01)
+y1 = np.sin(int(m)*2*pi*0.043*x)
+y2 = np.sin(int(m)*2*pi*0.031*x)
+y = y1+y2
+plt.plot(x,y)
+
+# Discrete with down sampling
+x = np.arange(-30, 30, 1)
+y1 = np.sin(int(m)*2*pi*0.043*x)
+y2 = np.sin(int(m)*2*pi*0.031*x)
+y = y1+y2
+plt.stem(x,y)
+
+# Draw both normal & down sampling: Continuous
+chart3 = plt.subplot(313)
 x = np.arange(-30, 30, 0.01)
 y1 = np.sin(2*pi*0.043*x)
 y2 = np.sin(2*pi*0.031*x)
@@ -17,7 +48,7 @@ y2 = np.sin(int(m)*2*pi*0.031*x)
 y = y1+y2
 plt.plot(x,y)
 
-#discrete
+# Draw both normal & down sampling: Discrete
 x = np.arange(-30, 30, 1)
 y1 = np.sin(2*pi*0.043*x)
 y2 = np.sin(2*pi*0.031*x)
